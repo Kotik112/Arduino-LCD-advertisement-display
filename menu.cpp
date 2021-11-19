01fd3338250a919ff7928671d84e674b0b80df07
UnnGledje <unn.gledje@gmail.com>
2021-11-19 15:23:35 +0100

    menu

---
+++ menu.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+
+#include "company.hpp"
+#include "serial.h"
+#include "ad_manager.hpp"
+#include "menu.hpp"
+
+
+using namespace std;
+
+
+int men_entry_choice(void) {
+
+	int choice;
+	std::cout << "\nchoose entry: ";
+	std::cin >> choice;
+	if (choice < SET || choice > EXT) {
+		std::cout << "not an option. try again." << endl;
+		return men_entry();
+	}
+	return choice;
+}
+
+int men_entry(void){
+
+std::cout << "#############################" << endl;
+std::cout << "####### MAIN MENU ###########" << endl;
+std::cout << "#     SELECT OPTION BELOW   #" << endl;
+std::cout << "#     1. ADD                #" << endl;
+std::cout << "#     2. RUN ADS            #" << endl;
+std::cout << "#     3. EXIT               #" << endl;
+std::cout << "#############################" << endl;
+    int staged = 0;
+    while(1) {
+        int choice = men_entry();
+        switch(choice) {
+            case SET:
+                if (staged == 1) {
+                    get_company_input();
+                    staged = 1;
+                } else {
+                    std::cout << "\nyou've staged all companies. let's run the ads!" << endl;
+                }
+                break;
+            case RUN:
+                if (staged == 2) {
+                    cout << "This is where we put function to run the shit" << endl;
+                    std::cout << "RUNNING THE SHOW" << endl;
+                    return 0;            
+                } else {
+                    std::cout << "Please declare messages and companies first" << endl;
+                    break;                
+                }
+            case EXT:
+                std::cout << "Good Bye" << endl;
+                exit(0);
+                return 0;
+            default:
+                std::cout << "Try again." << endl;                   
+        }
+    }
+    return 0;
+
+
+}
+
+
